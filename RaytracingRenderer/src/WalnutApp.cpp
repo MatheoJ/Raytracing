964bb8f7b414944f5e9ebd20c3e776c57bf4b388
MatheoJ <matheo.jospeph.01@gmail.com>
2023-11-11 13:55:45 -0500

    Addded accumulation

--- RaytracingRenderer/src/WalnutApp.cpp
+++ RaytracingRenderer/src/WalnutApp.cpp
@@ -46,7 +46,8 @@ public:
 
 	virtual void OnUpdate(float ts) override
 	{
-		m_Camera.OnUpdate(ts);
+		if (m_Camera.OnUpdate(ts))
+			m_Renderer.ResetFrameIndex();
 	}
 
 	virtual void OnUIRender() override
@@ -57,7 +58,13 @@ public:
 		{
 			Render();
 		}
+		ImGui::Checkbox("Accumulate", &m_Renderer.GetSettings().Accumulate);
+
+		if (ImGui::Button("Reset"))
+			m_Renderer.ResetFrameIndex();
+
 		ImGui::End();
+
 
 
 		ImGui::Begin("Scene");
